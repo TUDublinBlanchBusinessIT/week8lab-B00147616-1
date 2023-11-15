6244dd91e0bd9a5f2adee3caf2bf8f24e2cc29bb
B00147616 <B00147616@mytudublin.ie>
2023-11-15 10:06:42 +0000

    finished part 1

---
+++ CarPolicy.php
@@ -0,0 +1,30 @@
+<?php 
+
+class CarPolicy 
+{
+    private $policyNumber = 0;
+    private $yearlyPremuim = 0;
+    private $dateOfLastClaim = "";
+    
+    public function __construct($policyNumber, $yearlyPremuim)
+    {
+        $this->policyNumber = $policyNumber;         
+        $this->yearlyPremuim = $yearlyPremuim;
+    }
+    
+    public function setDateOfLastClaim($date)
+    
+    public function getTotalYearsNoClaims()
+{
+     $currentDate = new DateTime();
+     $lastDate= new DateTime($this->dateOfLastClaim);
+     $interval = $currentDate->diff($lastDate);
+     return $interval->format("%y");
+}
+    
+    public function __toString()
+    {
+        return $this->policyNumber;
+    }
+}
+?>
